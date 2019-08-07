3f74f6178775da8a8ed924c6c258672870187830
Aditya Karnik <aditya.s.karnik@gmail.com>
2019-08-07 09:55:22 +0530

    allowed hosted added of aws

--- web_crawler/settings.py
+++ web_crawler/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'mfrvrs#$^xd-f$3ah!f=*yqh7w4*+pfh1v@q1)*l%62&@y622!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','0.0.0.0']
+ALLOWED_HOSTS = ['127.0.0.1','0.0.0.0', '3.226.195.74']
 
 
 # Application definition
